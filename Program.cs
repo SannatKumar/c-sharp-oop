9c1c1731177c9c797da37912dc69e79245031b01
Raj Kumar <interplaytheheads@gmail.com>
2021-10-19 15:26:32 +0300

    message from user is printed 5 times

--- Program.cs
+++ Program.cs
@@ -11,11 +11,13 @@ namespace ooping_csharp
             myProgram.doSomething();
         }
 
-        public void doSomething()
+        public void doSomething()//void means no return type
         {
             User me = new User();
             me.Name = "Raj Kumar";
             me.LastName = "Tiwari";
+            string userMessage = me.Output(5);
+            Console.WriteLine(userMessage);
             me.Output(5);//prints the output 5 times from the function inside User classs
             //me.Output();//prints the output from the function inside User classs
             //Console.WriteLine(me.Name);
