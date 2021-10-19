f05f84b924b2c7fac5bcd72d41925ed6a428b191
Raj Kumar <interplaytheheads@gmail.com>
2021-10-19 20:23:46 +0300

    Calling the method to print the name

--- Program.cs
+++ Program.cs
@@ -14,10 +14,11 @@ namespace ooping_csharp
         public void doSomething()//void means no return type
         {
             User me = new User();
-            me.Name = "Raj Kumar";
+            //me.FirstName = "Raj Kumar";
             me.LastName = "Tiwari";
             string userMessage = me.Output(5);
             Console.WriteLine(userMessage);
+            Console.WriteLine(me.FirstName);
             me.Output(5);//prints the output 5 times from the function inside User classs
             //me.Output();//prints the output from the function inside User classs
             //Console.WriteLine(me.Name);
