59ae8bb39c769e23f967ca8b6218201e97b91651
Raj Kumar <interplaytheheads@gmail.com>
2021-10-21 12:52:21 +0300

    clsPerson created to store the data

---
+++ clsPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ooping_csharp
+{
+    public class clsPerson
+    {
+        public string FirstName;
+        public string MI;
+        public string LastName;
+    }
+}
